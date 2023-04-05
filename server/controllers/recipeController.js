2dc00e62ee04a0dd44e673ad679042cc33a687ae
Burak Güllüler <burakgulluler@gmail.com>
2023-04-05 22:25:29 +0300

    Explore Sections

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -82,3 +82,52 @@ exports.exploreRecipe = async(req, res) => {
     res.status(500).send({message: error.message || "Error Occured"});
   }
 }
+
+
+/*
+ * POST /search
+ * Search
+ */
+exports.searchRecipe = async(req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    let recipe = await Recipe.find({ $text: { $search: searchTerm, $diacriticSensitive: true}});
+    res.render('search', {title: 'Cooking Blog - Search', recipe});
+  } catch (error) {
+    res.status(500).send({message: error.message ||  'Error Occured'});
+  }
+
+} 
+
+
+/*
+ * GET /explore-latest
+ * Explore Latest
+ */
+
+exports.exploreLatest = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    res.render('explore-latest', {title: 'Cooking Blog - Explore Latest', recipe});
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured"});
+  }
+}
+
+
+/*
+ * GET /explore-random
+ * Explore Random
+ */
+
+exports.exploreRandom = async(req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', {title: 'Cooking Blog - Explore Random', recipe});
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured"});
+  }
+}
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -9,6 +9,9 @@ router.get("/",recipeController.homepage);
 router.get("/recipe/:id",recipeController.exploreRecipe);
 router.get("/categories",recipeController.exploreCategories);
 router.get("/categories/:id",recipeController.exploreCategoriesByID);
+router.post("/search",recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
 
 module.exports = router;
  
